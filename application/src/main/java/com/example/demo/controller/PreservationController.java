241ff4f6aaa63c9323631088417384100a63c169
Erick <39500821+wlater@users.noreply.github.com>
2021-01-06 09:56:00 -0300

    Initial commit

---
+++ application/src/main/java/com/example/demo/controller/PreservationController.java
@@ -0,0 +1,24 @@
+package com.example.demo.controller;
+
+import com.example.demo.UserDTO;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/local")
+public class PreservationController {
+
+    @GetMapping("/{RapMeta}")
+    public ResponseEntity<?> retriveData(@PathVariable("RapMeta")  String RapMeta) {
+        UserDTO user = new UserDTO()
+        .setName("joao")
+        .setCpf(RapMeta);
+        
+        return ResponseEntity.ok(user);
+    }
+
+}
---
+++ domain/src/main/java/com/example/demo/UserDTO.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Accessors(chain = true)
+public class UserDTO {
+
+    public String name;
+    public String cpf;
+    
+}
